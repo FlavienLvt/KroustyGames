f2e19b6edc5d20d33e1a92e413cc21947d39ff20
STUD - Leny Meziere <meziere@stud.uniza.sk>
2026-06-01 10:36:27 +0200

    feat: initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,8 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 8080, // Set the port to 8080
+    host: true, // Allow access from the network (optional)
+  },
 });
