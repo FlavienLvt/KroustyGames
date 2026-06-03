1861c7653d9066d4d4b1ae87b109179c08b5cb3b
AtlasV4 <lenymeziere@gmail.com>
2026-06-03 19:38:13 +0200

    fix: import clicker

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,6 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
 import MyGame from '../views/games/2048View.vue' 
-import KroustyClickerView from '../views/games/KroutsyClickerView.vue'
 import HomeView from '../views/HomeView.vue' // On importe la vraie page d'accueil
 
 const routes = [
